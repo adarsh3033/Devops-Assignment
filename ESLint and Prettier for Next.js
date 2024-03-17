bab06439254d4abf37c5f225f0b97d5e90ad4fd4
adarsh3033 <63419215+adarsh3033@users.noreply.github.com>
2024-03-17 16:10:27 +0530

    Add files via upload

---
+++ ESLint and Prettier for Next.js
@@ -0,0 +1,5 @@
+# Navigate to Next.js component directory
+cd MyApp/nextjs
+
+# Install ESLint and Prettier
+npm install --save-dev eslint prettier eslint-plugin-react eslint-plugin-react-hooks eslint-plugin-jsx-a11y @typescript-eslint/parser @typescript-eslint/eslint-plugin
